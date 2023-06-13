281c5068c6ab83cbbc29e23b10b259ada0131a1d
Amudacodes <ritzyimmortal@gmail.com>
2023-06-13 02:23:53 +0100

    BROCHURES page done

--- prezofinal-main/prezzo/src/lib/components/Nav.svelte
+++ prezofinal-main/prezzo/src/lib/components/Nav.svelte
@@ -6,9 +6,12 @@
     function toggleElement() {
       showElement = !showElement;
     }
-
+    function hideElement() {
+    showElement = false;
+  }
 
 </script>
+
 <div class="w-full h-[3.5rem] p-3 lg:py-0 fixed inset-0 bg-white lg:px-3 lg:h-[6rem] z-50">
     <div class='flex  justify-between w-full items-center lg:hidden'>
         <a href="/">
@@ -30,27 +33,27 @@
 
         <div id="element" class:show={showElement} class:return={!showElement} class='w-0 fixed flex flex-col bg-white pt-[4rem] lg:h-[6rem] h-full right-0 lg:relative lg:flex-row lg:p-0 lg:w-full lg:bg-transparent lg:justify-between lg:items-center ' >
             <a href="/">
-                <h1 class=' hidden text-3xl font-bold lg:block'>Prezzo</h1>
+                <h1 on:click={hideElement} class=' hidden text-3xl font-bold lg:block'>Prezzo</h1>
             </a>
     
             <div class="flex gap-4 font-medium lg:font-bold flex-col lg:flex-row pl-[2rem] lg:pl-0">
-                <a href="/" class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Home</a>
+                <a href="/" on:click={hideElement} class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Home</a>
     
-                <a href="/about" class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>About Us</a>
+                <a href="/about" on:click={hideElement} class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>About Us</a>
                 
-                <a href="/blog" class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>News</a>
+                <a href="/blog" on:click={hideElement} class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>News</a>
 
-                <a href="/howitworks" class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>Our Process</a>
+                <a href="/howitworks" on:click={hideElement} class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>Our Process</a>
 
-                <a href="/contact" class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>Contact Us</a>
+                <a href="/contact" on:click={hideElement} class='text-black font-medium lg:font-bold text-base transition-all delay-300 duration-300 hover:text-[#23286b]'>Contact Us</a>
 
-                <a href="/product" class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Product</a>
+                <a href="/product" on:click={hideElement} class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Product</a>
                 
-                <a href="/" class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Brochures</a>
+                <a href="/brochure" on:click={hideElement} class='text-black  text-base font-medium lg:font-bold transition-all delay-300 duration-300 hover:text-[#23286b]'>Brochures</a>
                
             </div>
     
-            <a href="/about" class="flex bg-[#1BC668] h-[2.5rem] w-[9rem] justify-center items-center text-white font-medium lg:font-bold ml-[2rem] lg:ml-0 mr-[5rem] lg:mr-0 mt-[1.5rem] lg:mt-0 transition-all delay-300 duration-300 hover:bg-[#23286b]">Learn More</a>
+            <a href="/about" on:click={hideElement} class="flex bg-[#1BC668] h-[2.5rem] w-[9rem] justify-center items-center text-white font-medium lg:font-bold ml-[2rem] lg:ml-0 mr-[5rem] lg:mr-0 mt-[1.5rem] lg:mt-0 transition-all delay-300 duration-300 hover:bg-[#23286b]">Learn More</a>
     
         </div>
     </div>
---
+++ prezofinal-main/prezzo/src/routes/brochure/+page.svelte
@@ -0,0 +1,209 @@
+<div class="bg-blue-100 text-gray-900 mt-[6rem]">
+    <!-- Hero Section -->
+    <section class="bg-[#23286b] py-12">
+      <div class="container mx-auto text-center">
+        <h1 class="text-3xl sm:text-3xl md:text-5xl font-bold text-white mb-4">Revolutionizing Medical Equipment</h1>
+        <p class="px-5 text-lg sm:text-xl md:text-2xl text-white">Discover our cutting-edge products that redefine healthcare.</p>
+        <a href="#explore" class="mt-6 inline-block px-8 py-3 rounded-lg bg-white text-indigo-600 font-semibold hover:bg-indigo-600 hover:text-white">Explore Now</a>
+      </div>
+    </section>
+  
+    <!-- Features Section -->
+    <section class="bg-gray-100 py-16" id="explore">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-8">Key Features</h2>
+        <div class="flex flex-wrap -mx-4">
+          <div class="md:w-1/2 lg:w-1/3 px-4 mb-8 ">
+            <!-- Apply hover-effect class -->
+            <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+              <svg class="w-10 h-10 p-1 mb-4 rounded-full bg-[#23286b] text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
+              <h3 class="text-xl font-semibold mb-2 hover:text-white">Advanced Technology</h3>
+              <p class="text-gray-700">Our equipment is equipped with advanced technology to ensure accurate and efficient results.</p>
+            </div>
+          </div>
+          <div class="md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <!-- Apply hover-effect class -->
+            <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+              <svg class="w-10 h-10 p-1 mb-4 rounded-full bg-[#23286b] text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <h3 class="text-xl font-semibold mb-2">Reliable Performance</h3>
+              <p class="text-gray-700">Our equipment is known for its reliable performance, ensuring accurate and consistent results.</p>
+            </div>
+          </div>
+          <div class="md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <!-- Apply hover-effect class -->
+            <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+              <svg class="w-11 h-11 mb-3 text-[#23286b]" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 100-12 6 6 0 000 12z" clip-rule="evenodd" />
+              </svg>
+              <h3 class="text-xl font-semibold mb-2">User-Friendly Design</h3>
+              <p class="text-gray-700">Our equipment is designed with the user in mind, providing an intuitive and user-friendly experience.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical-->
+  
+    <section class="bg-gray-200 py-12">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-6">VACCINE COLD CHAIN</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">VACCINE COLD CHAIN</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">REFRIGERATED VACCINE TRANSPORT VEHICLE</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue2.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical -->
+    <section class="bg-gray-100 py-16">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-8">MEDICAL REFRIGERATION</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">MEDICAL REFRIGERATION</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">LABORATORY REFRIGERATORS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category2.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">LABORATORY FREEZERS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">MEDICAL REFRIGERATORS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+  
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical -->
+    <section class="bg-gray-200 py-12">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-6">BLOOD MANAGEMENT</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">BLOOD MANAGEMENT SOLUTIONS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">BLOOD BANK REFRIGERATORS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue2.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">PLASMA STORAGE FREEZERS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">CONTACT SHOCK FREEZERS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">TRANSPORT SYSTEMS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical -->
+    <section class="bg-gray-100 py-16">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-8">TRANSPORT BOXES</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">TRANSPORT BOXES</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical -->
+    <section class="bg-gray-200 py-12">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-6">MEDICAL WASTE INCENERATORS</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">I8-75A-MODEL</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">I8-140-MODEL</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue2.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 crazy-hover">
+            <!-- Apply crazy-hover class -->
+            <h3 class="text-xl font-semibold mb-2">I8-250-MODEL</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/issue3.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    <!-- Medical -->
+    <section class="bg-gray-100 py-16">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-8">WALK-IN COLDROOMS</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">WALK-IN COLDROOM SUMMARY</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category1.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+          <div class="bg-white rounded-lg shadow-lg p-6 category-hover">
+            <!-- Apply category-hover class -->
+            <h3 class="text-xl font-semibold mb-2">WALK-IN COLDROOM SPECIFICATIONS</h3>
+            <p class="text-gray-700">Description</p>
+            <a href="pdf/category2.pdf" class="mt-4 inline-block px-8 py-3 rounded-lg bg-indigo-600 text-white font-semibold">Download PDF</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+  </div>
